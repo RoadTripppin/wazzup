35e8eeeeda802385ccafe92a5c4ccd3325bac539
Ashwin Gopinath <ashwing22@gmail.com>
2022-03-04 20:19:57 -0500

    Refactored and removed the old existing APIs after migration

--- api/api.go
+++ api/api.go
@@ -1,62 +1,16 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/RoadTripppin/wazzup/helpers"
-	"github.com/RoadTripppin/wazzup/models"
-	"github.com/RoadTripppin/wazzup/users"
+	"github.com/RoadTripppin/wazzup/controllers"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
-
-func login(w http.ResponseWriter, r *http.Request) {
-	// Read body
-	body, err := ioutil.ReadAll(r.Body)
-	helpers.HandleErr(err)
-	// Handle Login
-	var formattedBody models.Login
-	err = json.Unmarshal(body, &formattedBody)
-	helpers.HandleErr(err)
-	login := users.Login(formattedBody.Email, formattedBody.Password)
-	// Prepare response
-	if login["message"] == "all is fine" {
-		resp := login
-		json.NewEncoder(w).Encode(resp)
-		// Handle error in else
-	} else {
-		resp := models.ErrResponse{Message: "Wrong username or password"}
-		json.NewEncoder(w).Encode(resp)
-	}
-}
-
-func register(w http.ResponseWriter, r *http.Request) {
-	// Read body
-	body, err := ioutil.ReadAll(r.Body)
-	helpers.HandleErr(err)
-	// Handle registration
-	var formattedBody models.Register
-	err = json.Unmarshal(body, &formattedBody)
-	log.Println(formattedBody)
-	helpers.HandleErr(err)
-	register := users.Register(formattedBody.Name, formattedBody.Email, formattedBody.Password)
-	// Prepare response
-	log.Println(register)
-	if register["message"] == "all is fine" {
-		resp := register
-		json.NewEncoder(w).Encode(resp)
-		// Handle error in else
-	} else {
-		resp := models.ErrResponse{Message: "Wrong username or password"}
-		json.NewEncoder(w).Encode(resp)
-	}
-}
 
 func StartApi() {
 	err := godotenv.Load()
@@ -67,8 +21,8 @@ func StartApi() {
 
 	port := os.Getenv("SERVER_PORT")
 	router := mux.NewRouter()
-	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/register", register).Methods("POST")
+	router.HandleFunc("/login", controllers.Login).Methods("POST")
+	router.HandleFunc("/register", controllers.Register).Methods("POST")
 	fmt.Println("App is working on port :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 
