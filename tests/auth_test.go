f28f59296946d745aa3d78c15d810e626ad3deae
Shashank Bhat <s.2205.b@gmail.com>
2022-03-28 19:25:57 -0400

    remove redundant comments from test

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -24,13 +24,6 @@ func TestLoginSuccess(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-
-	// Check the response body is what we expect.
-	// expected := `[{"id":1,"first_name":"Krish","last_name":"Bhanushali","email_address":"krishsb@g.com","phone_number":"0987654321"},{"id":2,"first_name":"xyz","last_name":"pqr","email_address":"xyz@pqr.com","phone_number":"1234567890"},{"id":6,"first_name":"FirstNameSample","last_name":"LastNameSample","email_address":"lr@gmail.com","phone_number":"1111111111"}]`
-	// if rr.Body.String() != expected {
-	// 	t.Errorf("handler returned unexpected body: got %v want %v",
-	// 		rr.Body.String(), expected)
-	// }
 }
 
 func TestLoginFailureWithInvalidEmail(t *testing.T) {
