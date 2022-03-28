9995836302d29ef51b6175419735917c7e634e26
Shashank Bhat <s.2205.b@gmail.com>
2022-03-28 19:49:16 -0400

    login test with empty email

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -59,3 +59,20 @@ func TestLoginFailureWithInvalidPassword(t *testing.T) {
 			status, http.StatusUnauthorized)
 	}
 }
+
+func TestLoginFailureWithEmptyEmail(t *testing.T) {
+	var body = []byte(`{"email":"","password": "test123"}`)
+
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(body))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(controllers.Login)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnauthorized)
+	}
+}
