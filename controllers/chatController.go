62e21eaaf2029261a95beaff091362541890919a
Ashwin Gopinath <ashwing22@gmail.com>
2022-03-29 19:57:39 -0400

    One to one user chat API

--- controllers/chatController.go
+++ controllers/chatController.go
@@ -5,8 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	// Max wait time when writing message to peer
+	writeWait = 10 * time.Second
+
+	// Max time till next pong from peer
+	pongWait = 60 * time.Second
+
+	// Send ping interval, must be less then pong wait time
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 10000
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
 )
 
 var addr = flag.String("addr", ":8080", "http server address")
@@ -21,17 +42,98 @@ var upgrader = websocket.Upgrader{
 
 type Client struct {
 	// The actual websocket connection.
-	conn *websocket.Conn
+	conn     *websocket.Conn
+	wsServer *WsServer
+	send     chan []byte
 }
 
-func newClient(conn *websocket.Conn) *Client {
+func newClient(conn *websocket.Conn, wsServer *WsServer) *Client {
 	return &Client{
-		conn: conn,
+		conn:     conn,
+		wsServer: wsServer,
+		send:     make(chan []byte, 256),
 	}
 }
 
+func (client *Client) readPump() {
+	defer func() {
+		client.disconnect()
+	}()
+
+	client.conn.SetReadLimit(maxMessageSize)
+	client.conn.SetReadDeadline(time.Now().Add(pongWait))
+	client.conn.SetPongHandler(func(string) error { client.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
+	// Start endless read loop, waiting for messages from client
+	for {
+		_, jsonMessage, err := client.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("unexpected close error: %v", err)
+			}
+			break
+		}
+		fmt.Println(jsonMessage)
+		client.wsServer.broadcast <- jsonMessage
+	}
+}
+
+func (client *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		client.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-client.send:
+			fmt.Println("Message")
+
+			client.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The WsServer closed the channel.
+				client.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := client.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+
+			fmt.Printf("%s\n", reflect.TypeOf(message))
+			fmt.Println(string([]byte(message[:])))
+			w.Write(message)
+
+			// Attach queued chat messages to the current websocket message.
+			n := len(client.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-client.send)
+			}
+
+			fmt.Println(client.wsServer.clients)
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			client.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (client *Client) disconnect() {
+	client.wsServer.unregister <- client
+	close(client.send)
+	client.conn.Close()
+}
+
 // ServeWs handles websocket requests from clients requests.
-func ServeWs(w http.ResponseWriter, r *http.Request) {
+func ServeWs(wsServer *WsServer, w http.ResponseWriter, r *http.Request) {
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -39,8 +141,13 @@ func ServeWs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	client := newClient(conn)
+	client := newClient(conn, wsServer)
 
 	fmt.Println("New Client joined the hub!")
 	fmt.Println(client)
+
+	go client.writePump()
+	go client.readPump()
+
+	wsServer.register <- client
 }
