81a463c3dd52f3116ab512a92ade2560b487241a
Shashank Bhat <s.2205.b@gmail.com>
2022-03-29 20:35:30 -0400

    Add get user api

---
+++ helpers/users.go
@@ -0,0 +1,25 @@
+package helpers
+
+import "github.com/RoadTripppin/wazzup/models"
+
+func GetUser(email string) map[string]interface{} {
+	valid, field := Validation(
+		[]models.Validation{
+			{Value: email, Valid: "email"},
+		})
+
+	if valid {
+		db := ConnectDB()
+		user := &models.User{}
+		if db.Where("email = ? ", email).First(&user).RecordNotFound() {
+			return map[string]interface{}{"message": "User not found"}
+		}
+
+		defer db.Close()
+
+		var response = prepareGetUserResponse(user)
+		return response
+	} else {
+		return map[string]interface{}{"message": "Invalid " + field + " value"}
+	}
+}
